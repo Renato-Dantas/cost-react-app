6f0cc46a5866d373987ef677fce3160c5965d559
Renato Dantas <renatodantas@outlook.com.br>
2022-08-10 19:23:36 -0300

    Project Card Component

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,17 +1,27 @@
 import { useLocation } from "react-router-dom";
+import Container from "../layouts/Container";
 import Message from "../layouts/Message";
+import LinkButton from "../layouts/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import style from "../styles/projects/projects.module.scss";
+import { useState, useEffect } from "react";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
   const location = useLocation();
   let message = "";
-
   if (location.state) {
     message = location.state.message;
   }
+
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={style["project-container"]}>
+      <div className={style["title-container"]}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to={"/newproject"} text="Criar Projeto" />
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass={"start"}></Container>
     </div>
   );
 };
---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,8 @@
+import style from "../styles/projects/projectCard.module.scss";
+import { BsPencil, BsFillTrashFill } from "react-icons/bs";
+
+const ProjectCard = ({ id, name, budget, category, handleRemove }) => {
+  return <p>{name}</p>;
+};
+
+export default ProjectCard;
